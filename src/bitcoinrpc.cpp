b4e567f15da291e6aa08216d09452dcfd853b185
GIFcoin <eaheby@gmail.com>
2013-05-07 18:34:30 -0500

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -2843,7 +2843,7 @@ void ThreadRPCServer2(void* parg)
     {
         unsigned char rand_pwd[32];
         RAND_bytes(rand_pwd, 32);
-        string strWhatAmI = "To use gifcoin";
+        string strWhatAmI = "To use gifcoind";
         if (mapArgs.count("-server"))
             strWhatAmI = strprintf(_("To use the %s option"), "\"-server\"");
         else if (mapArgs.count("-daemon"))
